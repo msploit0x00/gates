7ce4850890e638834ae38aef2ff675a4cec48807
datasoft <datasoft2022@datasoft.com>
2022-07-26 16:37:47 +0200

    tickets

--- gates/gates/doctype/gate/gate.py
+++ gates/gates/doctype/gate/gate.py
@@ -8,37 +8,43 @@ from frappe.model.document import Document
 
 
 class Gate(Document):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    def after_insert(self):
         url = frappe.get_doc('Gate Attributes').url
-        headers = {
-                'Content-Type': 'application/json'
-        }
+        payload = json.dumps({
+            "gate": self.name
+        })
 
-        def after_insert(self):
-                payload = json.dumps({
-                        "gate": self.name
-                })
-                
-                response = requests.request("POST", self.url+'insertgate', headers=self.headers, data=payload)
-                self.render_response(response)
+        response = requests.request(
+            "POST", url+'insertgate', headers=self.headers, data=payload)
+        self.render_response(response)
 
-        def after_delete(self):
-                #TODO delete gate and devices related to this gate in django app
-                payload = json.dumps({
-                        "gate": self.name
-                })
-                response = requests.request("POST", self.url+'deletegate', headers=self.headers, data=payload)
-                self.render_response(response)
-                
-        def before_rename(self, old, new, merge):
-                print(f"old one {old} and the new one {new}")
-                payload = json.dumps({
-                        "old_name": old,
-                        "name": new
-                })
-                response = requests.request("POST", self.url+'updategate', headers=self.headers, data=payload)
-                self.render_response(response)
+    def after_delete(self):
+        # TODO delete gate and devices related to this gate in django app
+        url = frappe.get_doc('Gate Attributes').url
+        payload = json.dumps({
+            "gate": self.name
+        })
+        response = requests.request(
+            "POST", url+'deletegate', headers=self.headers, data=payload)
+        self.render_response(response)
 
-        def render_response(self, response):
-                resp = json.loads(response.text)
-                if not resp['success']:
-                        frappe.throw("Error happened")
+    def before_rename(self, old, new, merge):
+        url = frappe.get_doc('Gate Attributes').url
+        print(f"old one {old} and the new one {new}")
+        payload = json.dumps({
+            "old_name": old,
+            "name": new
+        })
+        response = requests.request(
+            "POST", url+'updategate', headers=self.headers, data=payload)
+        self.render_response(response)
+
+    def render_response(self, response):
+        url = frappe.get_doc('Gate Attributes').url
+        resp = json.loads(response.text)
+        if not resp['success']:
+            frappe.throw("Error happened")
--- gates/gates/doctype/zk_devices/zk_devices.py
+++ gates/gates/doctype/zk_devices/zk_devices.py
@@ -7,48 +7,57 @@ from frappe.model.document import Document
 
 
 class ZKDevices(Document):
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    def after_insert(self):
         url = frappe.get_doc('Gate Attributes').url
-        headers = {
-                'Content-Type': 'application/json'
-        }
+        payload = json.dumps({
+            "gate": self.gate,
+            "ip": self.ip,
+            "port": self.port,
+            "passwd": self.passwd or ''
+        })
+        response = requests.request(
+            "POST", url+'insertzk', headers=self.headers, data=payload)
+        self.render_response(response)
 
-        def after_insert(self):
-                payload = json.dumps({
-                        "gate": self.gate,
-                        "ip": self.ip,
-                        "port": self.port,
-                        "passwd": self.passwd or ''
-                })
-                response = requests.request("POST", self.url+'insertzk', headers=self.headers, data=payload)
-                self.render_response(response)
-                
-        def after_delete(self):
-                payload = json.dumps({
-                        "ip": self.ip
-                })
-                response = requests.request("POST", self.url+'deletezk', headers=self.headers, data=payload)
-                self.render_response(response)
-                
-        def before_rename(self, old, new, merge):
-                payload = json.dumps({
-                        "old_ip": old,
-                        "ip": new
-                })
-                response = requests.request("POST", self.url+'updatezk', headers=self.headers, data=payload)
-                self.render_response(response)
+    def after_delete(self):
+        url = frappe.get_doc('Gate Attributes').url
+        payload = json.dumps({
+            "ip": self.ip
+        })
+        response = requests.request(
+            "POST", url+'deletezk', headers=self.headers, data=payload)
+        self.render_response(response)
 
-        def on_update(self):
-                if self.creation != self.modified:
-                        payload = json.dumps({
-                                "old_ip": self.ip,
-                                "port": self.port,
-                                "gate": self.gate,
-                                "passwd": self.passwd
-                        });
-                        response = requests.request("POST", self.url+'updatezk', headers=self.headers, data=payload)
-                        self.render_response(response)
-                        
-        def render_response(self, response):
-                resp = json.loads(response.text)
-                if not resp['success']:
-                        frappe.throw("Error happened")
+    def before_rename(self, old, new, merge):
+        url = frappe.get_doc('Gate Attributes').url
+        payload = json.dumps({
+            "old_ip": old,
+            "ip": new
+        })
+        response = requests.request(
+            "POST", url+'updatezk', headers=self.headers, data=payload)
+        self.render_response(response)
+
+    def on_update(self):
+        url = frappe.get_doc('Gate Attributes').url
+        if self.creation != self.modified:
+            payload = json.dumps({
+                "old_ip": self.ip,
+                "port": self.port,
+                "gate": self.gate,
+                "passwd": self.passwd
+            })
+            response = requests.request(
+                "POST", url+'updatezk', headers=self.headers, data=payload)
+            self.render_response(response)
+
+    def render_response(self, response):
+        url = frappe.get_doc('Gate Attributes').url
+        resp = json.loads(response.text)
+        if not resp['success']:
+            frappe.throw("Error happened")
